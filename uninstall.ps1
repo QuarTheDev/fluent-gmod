46ce4f711e63b4884a22c83ade87cc6340bbaa5c
Quar <70469187+QuarTheDev@users.noreply.github.com>
2022-12-25 10:33:34 -0500

    Add files via upload

---
+++ uninstall.ps1
@@ -0,0 +1,359 @@
+Write-Output "Please wait..."
+# reg
+$wc = New-Object System.Net.WebClient
+
+# Paths #
+$downpath = "C:/Program Files (x86)/Steam/steamapps/common/GarrysMod/garrysmod/html/img/"
+$ra = "https://raw.githubusercontent.com/QuarTheDev/fluent-gmod/main/default_icons/"
+$path1 = "addonpreview.png"
+$path2 = "addons.png"
+$path3 = "arrow_left.png"
+$path4 = "arrow_right.png"
+$path5 = "back.png"
+$path6 = "back_to_game.png"
+$path7 = "back_to_main_menu.png"
+$path8 = "bg.jpg"
+$path9 = "bg.png"
+$path10 = "bg_arrow_left.png"
+$path11 = "bg_dark.jpg"
+$path12 = "bg_dark.png"
+$path13 = "close.png"
+$path14 = "creation-play.png"
+$path15 = "creations.png"
+$path16 = "disable.png"
+$path17 = "disconnect.png"
+$path18 = "downloading.png"
+$path19 = "empty.png"
+$path20 = "enabled.png"
+$path21 = "error.png"
+$path22 = "favourite.png"
+$path23 = "favourite_add.png"
+$path24 = "favourite_remove.png"
+$path25 = "favourite_server.png"
+$path26 = "favourite_server_del.png"
+$path27 = "games.png"
+$path28 = "gm_install16.png"
+$path29 = "gm_install32.png"
+$path30 = "gmod_logo_brave.png"
+$path31 = "gradient.png"
+$path32 = "incompatible.png"
+$path33 = "install.png"
+$path34 = "kinect.png"
+$path35 = "last.png"
+$path36 = "localaddon.png"
+$path37 = "news.png"
+$path38 = "next.png"
+$path39 = "notfavourite.png"
+$path40 = "notfavourite_add.png"
+$path41 = "notinstalled.png"
+$path42 = "notowned.png"
+$path43 = "overlay-demo.png"
+$path44 = "overlay-save.png"
+$path45 = "page_active.png"
+$path46 = "pagination_page.png"
+$path47 = "play.png"
+$path48 = "plus.png"
+$path49 = "rank1.png"
+$path50 = "rank2.png"
+$path51 = "rank3.png"
+$path52 = "rank4.png"
+$path53 = "rank5.png"
+$path54 = "record_movie.png"
+$path55 = "remove.png"
+$path56 = "render.png"
+$path57 = "server-dedicated.png"
+$path58 = "server-passworded.png"
+$path59 = "settings.png"
+$path60 = "subscribe.png"
+$path61 = "thumb-down.png"
+$path62 = "thumb-up.png"
+$path63 = "tick.png"
+$path64 = "unk_flag.png"
+$path65 = "unsubscribe.png"
+$path66 = "upload.png"
+$path67 = "viewonline.png"
+$path68 = "whatsnew.png"
+$path69 = "workshop.png"
+# Paths #
+Write-Output "OK"
+
+cls
+
+Write-Host -NoNewline "[1/69] Downloading $path1......."
+$wc.DownloadFile("$ra"+""+"$path1", "$downpath"+""+"$path1")
+Write-Host "done"
+
+Write-Host -NoNewline "[2/69] Downloading $path2......."
+$wc.DownloadFile("$ra"+""+"$path2", "$downpath"+""+"$path2")
+Write-Host "done"
+
+Write-Host -NoNewline "[3/69] Downloading $path3......."
+$wc.DownloadFile("$ra"+""+"$path3", "$downpath"+""+"$path3")
+Write-Host "done"
+
+Write-Host -NoNewline "[4/69] Downloading $path4......."
+$wc.DownloadFile("$ra"+""+"$path4", "$downpath"+""+"$path4")
+Write-Host "done"
+
+Write-Host -NoNewline "[5/69] Downloading $path5......."
+$wc.DownloadFile("$ra"+""+"$path5", "$downpath"+""+"$path5")
+Write-Host "done"
+
+Write-Host -NoNewline "[6/69] Downloading $path6......."
+$wc.DownloadFile("$ra"+""+"$path6", "$downpath"+""+"$path6")
+Write-Host "done"
+
+Write-Host -NoNewline "[7/69] Downloading $path7......."
+$wc.DownloadFile("$ra"+""+"$path7", "$downpath"+""+"$path7")
+Write-Host "done"
+
+Write-Host -NoNewline "[8/69] Downloading $path8......."
+$wc.DownloadFile("$ra"+""+"$path8", "$downpath"+""+"$path8")
+Write-Host "done"
+
+Write-Host -NoNewline "[9/69] Downloading $path9......."
+$wc.DownloadFile("$ra"+""+"$path9", "$downpath"+""+"$path9")
+Write-Host "done"
+
+Write-Host -NoNewline "[10/69] Downloading $path10......."
+$wc.DownloadFile("$ra"+""+"$path10", "$downpath"+""+"$path10")
+Write-Host "done"
+
+Write-Host -NoNewline "[11/69] Downloading $path11......."
+$wc.DownloadFile("$ra"+""+"$path11", "$downpath"+""+"$path11")
+Write-Host "done"
+
+Write-Host -NoNewline "[12/69] Downloading $path12......."
+$wc.DownloadFile("$ra"+""+"$path12", "$downpath"+""+"$path12")
+Write-Host "done"
+
+Write-Host -NoNewline "[13/69] Downloading $path13......."
+$wc.DownloadFile("$ra"+""+"$path13", "$downpath"+""+"$path13")
+Write-Host "done"
+
+Write-Host -NoNewline "[14/69] Downloading $path14......."
+$wc.DownloadFile("$ra"+""+"$path14", "$downpath"+""+"$path14")
+Write-Host "done"
+
+Write-Host -NoNewline "[15/69] Downloading $path15......."
+$wc.DownloadFile("$ra"+""+"$path15", "$downpath"+""+"$path15")
+Write-Host "done"
+
+Write-Host -NoNewline "[16/69] Downloading $path16......."
+$wc.DownloadFile("$ra"+""+"$path16", "$downpath"+""+"$path16")
+Write-Host "done"
+
+Write-Host -NoNewline "[17/69] Downloading $path17......."
+$wc.DownloadFile("$ra"+""+"$path17", "$downpath"+""+"$path17")
+Write-Host "done"
+
+Write-Host -NoNewline "[18/69] Downloading $path18......."
+$wc.DownloadFile("$ra"+""+"$path18", "$downpath"+""+"$path18")
+Write-Host "done"
+
+Write-Host -NoNewline "[19/69] Downloading $path19......."
+$wc.DownloadFile("$ra"+""+"$path19", "$downpath"+""+"$path19")
+Write-Host "done"
+
+Write-Host -NoNewline "[20/69] Downloading $path20......."
+$wc.DownloadFile("$ra"+""+"$path20", "$downpath"+""+"$path20")
+Write-Host "done"
+
+Write-Host -NoNewline "[21/69] Downloading $path21......."
+$wc.DownloadFile("$ra"+""+"$path21", "$downpath"+""+"$path21")
+Write-Host "done"
+
+Write-Host -NoNewline "[22/69] Downloading $path22......."
+$wc.DownloadFile("$ra"+""+"$path22", "$downpath"+""+"$path22")
+Write-Host "done"
+
+Write-Host -NoNewline "[23/69] Downloading $path23......."
+$wc.DownloadFile("$ra"+""+"$path23", "$downpath"+""+"$path23")
+Write-Host "done"
+
+Write-Host -NoNewline "[24/69] Downloading $path24......."
+$wc.DownloadFile("$ra"+""+"$path24", "$downpath"+""+"$path24")
+Write-Host "done"
+
+Write-Host -NoNewline "[25/69] Downloading $path25......."
+$wc.DownloadFile("$ra"+""+"$path25", "$downpath"+""+"$path25")
+Write-Host "done"
+
+Write-Host -NoNewline "[26/69] Downloading $path26......."
+$wc.DownloadFile("$ra"+""+"$path26", "$downpath"+""+"$path26")
+Write-Host "done"
+
+Write-Host -NoNewline "[27/69] Downloading $path27......."
+$wc.DownloadFile("$ra"+""+"$path27", "$downpath"+""+"$path27")
+Write-Host "done"
+
+Write-Host -NoNewline "[28/69] Downloading $path28......."
+$wc.DownloadFile("$ra"+""+"$path28", "$downpath"+""+"$path28")
+Write-Host "done"
+
+Write-Host -NoNewline "[29/69] Downloading $path29......."
+$wc.DownloadFile("$ra"+""+"$path29", "$downpath"+""+"$path29")
+Write-Host "done"
+
+Write-Host -NoNewline "[30/69] Downloading $path30......."
+$wc.DownloadFile("$ra"+""+"$path30", "$downpath"+""+"$path30")
+Write-Host "done"
+
+Write-Host -NoNewline "[31/69] Downloading $path31......."
+$wc.DownloadFile("$ra"+""+"$path31", "$downpath"+""+"$path31")
+Write-Host "done"
+
+Write-Host -NoNewline "[32/69] Downloading $path32......."
+$wc.DownloadFile("$ra"+""+"$path32", "$downpath"+""+"$path32")
+Write-Host "done"
+
+Write-Host -NoNewline "[33/69] Downloading $path33......."
+$wc.DownloadFile("$ra"+""+"$path33", "$downpath"+""+"$path33")
+Write-Host "done"
+
+Write-Host -NoNewline "[34/69] Downloading $path34......."
+$wc.DownloadFile("$ra"+""+"$path34", "$downpath"+""+"$path34")
+Write-Host "done"
+
+Write-Host -NoNewline "[35/69] Downloading $path35......."
+$wc.DownloadFile("$ra"+""+"$path35", "$downpath"+""+"$path35")
+Write-Host "done"
+
+Write-Host -NoNewline "[36/69] Downloading $path36......."
+$wc.DownloadFile("$ra"+""+"$path36", "$downpath"+""+"$path36")
+Write-Host "done"
+
+Write-Host -NoNewline "[37/69] Downloading $path37......."
+$wc.DownloadFile("$ra"+""+"$path37", "$downpath"+""+"$path37")
+Write-Host "done"
+
+Write-Host -NoNewline "[38/69] Downloading $path38......."
+$wc.DownloadFile("$ra"+""+"$path38", "$downpath"+""+"$path38")
+Write-Host "done"
+
+Write-Host -NoNewline "[39/69] Downloading $path39......."
+$wc.DownloadFile("$ra"+""+"$path39", "$downpath"+""+"$path39")
+Write-Host "done"
+
+Write-Host -NoNewline "[40/69] Downloading $path40......."
+$wc.DownloadFile("$ra"+""+"$path40", "$downpath"+""+"$path40")
+Write-Host "done"
+
+Write-Host -NoNewline "[41/69] Downloading $path41......."
+$wc.DownloadFile("$ra"+""+"$path41", "$downpath"+""+"$path41")
+Write-Host "done"
+
+Write-Host -NoNewline "[42/69] Downloading $path42......."
+$wc.DownloadFile("$ra"+""+"$path42", "$downpath"+""+"$path42")
+Write-Host "done"
+
+Write-Host -NoNewline "[43/69] Downloading $path43......."
+$wc.DownloadFile("$ra"+""+"$path43", "$downpath"+""+"$path43")
+Write-Host "done"
+
+Write-Host -NoNewline "[44/69] Downloading $path44......."
+$wc.DownloadFile("$ra"+""+"$path44", "$downpath"+""+"$path44")
+Write-Host "done"
+
+Write-Host -NoNewline "[45/69] Downloading $path45......."
+$wc.DownloadFile("$ra"+""+"$path45", "$downpath"+""+"$path45")
+Write-Host "done"
+
+Write-Host -NoNewline "[46/69] Downloading $path46......."
+$wc.DownloadFile("$ra"+""+"$path46", "$downpath"+""+"$path46")
+Write-Host "done"
+
+Write-Host -NoNewline "[47/69] Downloading $path47......."
+$wc.DownloadFile("$ra"+""+"$path47", "$downpath"+""+"$path47")
+Write-Host "done"
+
+Write-Host -NoNewline "[48/69] Downloading $path48......."
+$wc.DownloadFile("$ra"+""+"$path48", "$downpath"+""+"$path48")
+Write-Host "done"
+
+Write-Host -NoNewline "[49/69] Downloading $path49......."
+$wc.DownloadFile("$ra"+""+"$path49", "$downpath"+""+"$path49")
+Write-Host "done"
+
+Write-Host -NoNewline "[50/69] Downloading $path50......."
+$wc.DownloadFile("$ra"+""+"$path50", "$downpath"+""+"$path50")
+Write-Host "done"
+
+Write-Host -NoNewline "[51/69] Downloading $path51......."
+$wc.DownloadFile("$ra"+""+"$path51", "$downpath"+""+"$path51")
+Write-Host "done"
+
+Write-Host -NoNewline "[52/69] Downloading $path52......."
+$wc.DownloadFile("$ra"+""+"$path52", "$downpath"+""+"$path52")
+Write-Host "done"
+
+Write-Host -NoNewline "[53/69] Downloading $path53......."
+$wc.DownloadFile("$ra"+""+"$path53", "$downpath"+""+"$path53")
+Write-Host "done"
+
+Write-Host -NoNewline "[54/69] Downloading $path54......."
+$wc.DownloadFile("$ra"+""+"$path54", "$downpath"+""+"$path54")
+Write-Host "done"
+
+Write-Host -NoNewline "[55/69] Downloading $path55......."
+$wc.DownloadFile("$ra"+""+"$path55", "$downpath"+""+"$path55")
+Write-Host "done"
+
+Write-Host -NoNewline "[56/69] Downloading $path56......."
+$wc.DownloadFile("$ra"+""+"$path56", "$downpath"+""+"$path56")
+Write-Host "done"
+
+Write-Host -NoNewline "[57/69] Downloading $path57......."
+$wc.DownloadFile("$ra"+""+"$path57", "$downpath"+""+"$path57")
+Write-Host "done"
+
+Write-Host -NoNewline "[58/69] Downloading $path58......."
+$wc.DownloadFile("$ra"+""+"$path58", "$downpath"+""+"$path58")
+Write-Host "done"
+
+Write-Host -NoNewline "[59/69] Downloading $path59......."
+$wc.DownloadFile("$ra"+""+"$path59", "$downpath"+""+"$path59")
+Write-Host "done"
+
+Write-Host -NoNewline "[60/69] Downloading $path60......."
+$wc.DownloadFile("$ra"+""+"$path60", "$downpath"+""+"$path60")
+Write-Host "done"
+
+Write-Host -NoNewline "[61/69] Downloading $path61......."
+$wc.DownloadFile("$ra"+""+"$path61", "$downpath"+""+"$path61")
+Write-Host "done"
+
+Write-Host -NoNewline "[62/69] Downloading $path62......."
+$wc.DownloadFile("$ra"+""+"$path62", "$downpath"+""+"$path62")
+Write-Host "done"
+
+Write-Host -NoNewline "[63/69] Downloading $path63......."
+$wc.DownloadFile("$ra"+""+"$path63", "$downpath"+""+"$path63")
+Write-Host "done"
+
+Write-Host -NoNewline "[64/69] Downloading $path64......."
+$wc.DownloadFile("$ra"+""+"$path64", "$downpath"+""+"$path64")
+Write-Host "done"
+
+Write-Host -NoNewline "[65/69] Downloading $path65......."
+$wc.DownloadFile("$ra"+""+"$path65", "$downpath"+""+"$path65")
+Write-Host "done"
+
+Write-Host -NoNewline "[66/69] Downloading $path66......."
+$wc.DownloadFile("$ra"+""+"$path66", "$downpath"+""+"$path66")
+Write-Host "done"
+
+Write-Host -NoNewline "[67/69] Downloading $path67......."
+$wc.DownloadFile("$ra"+""+"$path67", "$downpath"+""+"$path67")
+Write-Host "done"
+
+Write-Host -NoNewline "[68/69] Downloading $path68......."
+$wc.DownloadFile("$ra"+""+"$path68", "$downpath"+""+"$path68")
+Write-Host "done"
+
+Write-Host -NoNewline "[69/69] Downloading $path69......."
+$wc.DownloadFile("$ra"+""+"$path69", "$downpath"+""+"$path69")
+Write-Host "done"
+Write-Host ""
+Write-Host "Fluent Garry's Mod has been removed and defaults have been restored."
+Write-Host "Thanks for trying out Fluent Garry's Mod!"
